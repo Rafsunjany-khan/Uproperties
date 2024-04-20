5c512dc28e404ad39f4ead7a9f10a0500253883a
Rafsunjany_khan <rafsunjanykhan21@gmail.com>
2024-04-21 01:21:15 +0600

    modify some menu & urls

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -6,6 +6,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',views.homepage, name='home'),
     path('contact/',views.contact, name='contact'),
+    path('about/',views.about, name='about'),
+    path('service/',views.service, name='service'),
+    path('properties/',views.properties, name='properties'),
+    path('blog/',views.blog, name='blog'),
+    path('add-properties/',views.addProperties, name='add-properties'),
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mainApp/views.py
+++ mainApp/views.py
@@ -3,5 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, 'index.html')
-def contact(reuest):
-    return render(reuest, 'contact.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+def about(request):
+    return render(request, 'about.html')
+
+def properties(request):
+    return render(request, 'properties.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def service(request):
+    return render(request, 'services.html')
+
+def addProperties(request):
+    return render(request, 'add-properties.html')
